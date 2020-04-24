34fc1331b653deb6d990974a5a90230e485636a9
Couson <couson@foxmail.com>
2020-04-23 19:50:36 -0700

    warpping

--- API.py
+++ API.py
@@ -11,110 +11,157 @@ from datetime import datetime
 #import cgitb
 import cgi, cgitb
 #cgitb.enable()
-print("Content-Type: text/html")
-print("")
-
-print("DSC190 API")
 
 
+def execute_sql(sql_cmd, cursor):
+    cursor.execute(sql_cmd)
+    res = cursor.fetchall()
+    return res
 
-#GET URL, see my example urls at bottom, we write four functions together
-if os.environ['REQUEST_METHOD']=='GET':
-    pass
-else:
-    print('only GET is supportted')
-    sys.exit(0)
-params = cgi.FieldStorage()
-time=datetime.now()
-time=time.strftime('%Y-%m-%d %H:%M:%S')
 
-# connect DB in the server
-connection = pymysql.connect(host='localhost',
-                             user='iotdev',
-                             password='iotdb190',
-                             db='iotdb',
-                             cursorclass=pymysql.cursors.DictCursor
-                             )
+def display_data(data, title = ''):
+    data = str(data)
+    data = data.replace("u\'", "\'")
+    data = data.replace("\'", "\"")
+    print('{"%s" : ' % title)
+    print(data)
+    print('}')
 
-cursor = connection.cursor()
 
-if params['cmd'].value == "LIST":
+def format_result(fields, args):
+    assert len(fields) == len(args), 'length mismatch!'
+
+    result = {}
+    for i in range(len(fields)):
+        result[fields[i]] = args[i]
+    
+    print(result)
+
+
+def get_global_time(time_format = '%Y-%m-%d %H:%M:%S'):
+    out = datetime.now().strftime(time_format)
+    return out
+
+
+def get_list(params, cursor):
     if "gid" not in params.keys():
         sql = "SELECT * FROM iotdb.devices"
     else:
-        sql= "SELECT * FROM iotdb.devices WHERE groupID="+ params['gid'].value
-    cursor.execute(sql)
-    data = cursor.fetchall()
+        sql= "SELECT * FROM iotdb.devices WHERE groupID=%s" % params['gid'].value
+
+    data = execute_sql(sql, cursor)
     for item in data:
        item['lastseen'] = str(item['lastseen'])
-    data = str(data)
-    data = data.replace("u\'", "\'")
-    data = data.replace("\'", "\"")
-    print('{"devices" : ')
-    print(data)
-    print('}')
+    
+    display_data(data, 'devices')
 
-if params['cmd'].value == "GROUPS":
-    if "gid" not in params.keys():
+
+def get_groups(params, cursor):
+    if 'gid' not in params.keys():
         sql = "SELECT groupName,Student_1,Student_2 FROM iotdb.groups"
     else:
-        sql= "SELECT groupName,Student_1,Student_2 FROM iotdb.groups WHERE groupID=" + params['gid'].value
-    cursor.execute(sql)
-    data = cursor.fetchall()
-    data = str(data)
-    data = data.replace("u\'", "\'")
-    data = data.replace("\'", "\"")
-    print('{"groups" : ')
-    print(data)
-    print('}')
+        sql= "SELECT groupName,Student_1,Student_2 FROM iotdb.groups WHERE groupID=%s" % params['gid'].value
+    data = execute_sql(sql, cursor)
+    display_data(data, 'groups')
+    
 
-if params['cmd'].value == 'REG':
+def get_reg(params, cursor):
     mac = params['mac'].value
-    check_sql = "SELECT * FROM iotdb.devices WHERE iotdb.devices.mac = '%s'" % mac
-    cursor.execute(check_sql)
-    data=cursor.fetchall()
-    # if the device is not registered
-    if len(data)<1:
+    query = "SELECT * FROM iotdb.devices WHERE iotdb.devices.mac = '%s'" % mac
+    data = execute_sql(query, cursor)
+
+    # if the device is not registered, insert the new
+    if len(data) < 1:
         divi_sql = "INSERT INTO iotdb.devices(mac, groupID, lastseen) \
-                    VALUES('{0}', '{1}', '{2}')".format(mac,params['gid'].value, time)
+                    VALUES('{0}', '{1}', '{2}')".format(mac, 
+                    params['gid'].value, 
+                    time)
+    # if an existing device, update the origin
     else:
         divi_sql="UPDATE iotdb.devices \
             SET lastseen='{0}', groupID='{1}' \
-            WHERE mac='{2}'".format(time, params['gid'].value, mac) 
+            WHERE mac='{2}'".format(time,
+             params['gid'].value, 
+             mac) 
     try:
-        cursor.execute(divi_sql)
+        execute_sql(divi_sql, cursor)
         connection.commit()
-        status='OK'
-    except Exception as e:
-        status='failed'
-        print(e)
-    result={}
-    result['mac']=mac
-    result['time']=time
-    result['status']=status
-    print(result)
+        status='successed'
+        
+    except Exception as err:
+        status = 'failed'
+        print(err)
+
+    format_result(['mac', 'time', 'status'], [mac, time, status])
 
 
-
-if params['cmd'].value == 'LOG':
+def get_log(params, cursor):
     mac = params['mac'].value
-    sql = "INSERT INTO iotdb.testlogs(mac, ts, temp, hum) \
-           VALUES('{0}', '{1}', '{2}', '{3}')".format(mac, time, params['t'].value, params['h'].value)
-    try:
-        cursor.execute(sql)
-        connection.commit()
-        status='OK'
-    except Exception as e:
-        print(e)
-        status="failed"
-    result={}
-    result['timestamp']=time
-    result['status']=status
-    print(result)
-    
-cursor.close()
-connection.close()
+    temp = params['t'].value
+    hum = params['h'].value
 
+    sql = "INSERT INTO iotdb.testlogs(mac, ts, temp, hum) \
+           VALUES('{0}', '{1}', '{2}', '{3}')".format(mac, time, temp, hum)
+    try:
+        execute_sql(sql, cursor)
+        connection.commit()
+
+        status='successed'
+    except Exception as err:
+        print(err)
+        status="failed"
+
+    format_result(['timestamp', 'status'], [time, status])
+
+
+def main():
+
+    print("Content-Type: text/html")
+    print("")
+    print("DSC190 API version 0.0.3")
+
+    #GET URL, see my example urls at bottom, we write four functions together
+    if os.environ['REQUEST_METHOD']=='GET':
+        pass
+    else:
+        print('only GET is supportted')
+        sys.exit(0)
+    params = cgi.FieldStorage()
+    # print(params.value)
+    global time
+    time = get_global_time()
+
+    # connect DB in the server
+    global connection
+    connection = pymysql.connect(host='localhost',
+                                user='iotdev',
+                                password='iotdb190',
+                                db='iotdb',
+                                cursorclass=pymysql.cursors.DictCursor
+                                )
+
+    cursor = connection.cursor()
+
+    if params['cmd'].value.upper() == 'LIST':
+        get_list(params, cursor)
+
+    if params['cmd'].value.upper() == 'GROUPS':
+        get_groups(params, cursor)
+
+    if params['cmd'].value.upper() == 'REG':
+        get_reg(params, cursor)
+
+    if params['cmd'].value.upper() == 'LOG':
+        get_log(params, cursor)
+
+    cursor.close()
+    connection.close()
+
+
+if __name__ == '__main__':
+    main()
+
+    
 """
 examples: (All methods are GET)
 http://dsc-iot.ucsd.edu/gid03/cgi-bin/API.py?cmd=LIST
