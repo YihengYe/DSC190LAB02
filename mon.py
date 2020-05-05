9ff4afa5832e2f5d23a247f30ca0e5bf5b691e1a
YihengYe <39888273+YihengYe@users.noreply.github.com>
2020-05-06 02:45:18 +0800

    mon stack

--- mon.py
+++ mon.py
@@ -10,10 +10,31 @@ from datetime import datetime
 import time
 
 
+
+connection = pymysql.connect(host='localhost',
+                                user='iotdev',
+                                password='iotdb190',
+                                db='iotdb',
+                                cursorclass=pymysql.cursors.DictCursor
+                                )
+
+cursor = connection.cursor()
+
 def process_db():
 	now = datetime.now()
+	timeout='TIMEOUT'
+	error='ERROR'
+	ok='OK'
+	warning='WARNING'
 	print(now.strftime("%Y-%m-%d %H:%M:%S")," I'm checking for timeout")
-	
+	sql='SELECT * FROM iotdb.devices WHERE groupID=3'
+	cursor.excute(sql)
+	devs=cursor.fetchall()
+	for dev in devs:
+		mac=dev['mac']
+		sql_timeout="UPDATE iotdb.devices SET status='{0}'\
+			 WHERE mac='{1}' AND groupID=3 ".format(TIMEOUT, mac)
+
 
 
 
@@ -21,4 +42,6 @@ def process_db():
 while True:
 	process_db()
 	time.sleep(5)
+
+connection.close()
 
