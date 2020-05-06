54772dce510838ffd9af322b6022a156dbb21105
YihengYe <39888273+YihengYe@users.noreply.github.com>
2020-05-06 09:47:22 +0800

    first try

--- mon.py
+++ mon.py
@@ -27,22 +27,53 @@ def process_db():
 	ok='OK'
 	warning='WARNING'
 	print(now.strftime("%Y-%m-%d %H:%M:%S")," I'm checking for timeout")
-	sql='SELECT * FROM iotdb.devices WHERE groupID=3'
+	sql='SELECT DISTINCT blemac FROM iotdb.blelogs WHERE gid=3'
 	cursor.excute(sql)
 	devs=cursor.fetchall()
 	for dev in devs:
-		mac=dev['mac']
+		mac=dev['blemac']
 		sql_timeout="UPDATE iotdb.devices SET status='{0}'\
 			 WHERE now()> DATE_ADD(lastseen, INTERVAL 30 SECOND) AND mac='{1}' AND groupID=3 ".format(timeout, mac)
 		sql_error=sql_timeout="UPDATE iotdb.devices SET status='{0}'\
-			 WHERE now()> DATE_ADD(lastseen, INTERVAL 90 SECOND) AND mac='{1}' AND groupID=3 AND status='{2}'"
-			 .format(error, mac, timeout)
+			 WHERE now()> DATE_ADD(lastseen, INTERVAL 60 SECOND) \
+				 AND mac='{1}' AND groupID=3 AND status='{2}'".format(error, mac, timeout)
+		sql2="SELECT status FROM iotdb.devices WHERE groupID=3 AND mac='{0}'".format(mac)
+		cursor.excute(sql2)
+		st1=cursor.fetchall()[0]
+		st=st1['status']
+		if st==timeout or st==error:
+			sql3="SELECT * FROM iotdb.blelogs WHERE gid=3 AND now()< DATE_ADD(timestamp, INTERVAL 15 SECOND)\
+				AND mac='{0}'".format(mac)
+			cursor.excute(sql3)
+			data=cursor.fetchall()
+			if len(data)>=1:
+				sql_warning="UPDATE iotdb.devices SET status='{0}'\
+			 		WHERE mac='{1}' AND groupID=3".format(warning, mac)
+				cursor.excute(sql_warning)
+				cursor.commit()
+
+		elif st==warning:
+			sql4="SELECT * FROM iotdb.blelogs WHERE gid=3 AND now()< DATE_ADD(timestamp, INTERVAL 30 SECOND)\
+				AND mac='{0}'".format(mac)
+			cursor.excute(sql4)
+			data=cursor.fetchall()
+			if len(data)>=3:
+				sql_ok="UPDATE iotdb.devices SET status='{0}'\
+			 		WHERE mac='{1}' AND groupID=3".format(ok, mac)
+				cursor.excute(sql_ok)
+				cursor.commit()
+		else:
+			cursor.excute(sql_timeout)
+			cursor.commit()
+			cursor.excute(sql_error)
+			cursor.commit()
 
 
 
 
 
 #========== MAIN ===========
+
 while True:
 	process_db()
 	time.sleep(5)
