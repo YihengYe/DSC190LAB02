fbe9534b56734bef0a064faa49211497d8b901d1
Couson <couson@foxmail.com>
2020-05-19 04:30:51 -0700

    update API

--- API.py
+++ API.py
@@ -364,8 +364,10 @@ def main():
 
     if cmd_line == 'LOGDEV':
         post_logdev(params, cursor, GET)
+
     if cmd_line =='BLELIST':
         get_blelist(params, cursor, GET)
+        
     if cmd_line =='DEVLIST':
         get_devlist(params, cursor, GET)
 
